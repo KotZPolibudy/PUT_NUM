b30349197ce1423f193e98c1b502e8cf99f791fc
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-03-29 23:46:29 +0100

    v0.1

---
+++ Deploy/src/client.py
@@ -0,0 +1,11 @@
+import bentoml
+
+with bentoml.SyncHTTPClient("http://localhost:3000") as client:
+    result = client.predict(
+        input_data=[
+            [5.9, 3, 5.1, 1.8]
+        ],
+    )
+    print(result)
+
+    # todo load image and client.predict that
