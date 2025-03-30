6278d278625abaf3f6ee81bc6030b71d261dbfa2
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-03-30 23:56:18 +0200

    service updated for image data

--- Deploy/src/service.py
+++ Deploy/src/service.py
@@ -1,29 +1,50 @@
 import bentoml
+import base64
+import io
+from PIL import Image
 import numpy as np
-
+import torch
+import torchvision.transforms as transforms
 from bentoml.models import BentoModel
 
-# Define the runtime environment for your Bento
-demo_image = bentoml.images.PythonImage(python_version="3.11") \
-    .python_packages("mlflow", "scikit-learn")
+# Definicja obrazu runtime
+image_runtime = bentoml.images.PythonImage(python_version="3.11") \
+    .python_packages("mlflow", "torch", "torchvision", "Pillow", "numpy")
 
-target_names = ['setosa', 'versicolor', 'virginica']
+# Transformacja obrazu
+transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((64, 64)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
 
 @bentoml.service(
-    image=demo_image,
+    image=image_runtime,
     resources={"cpu": "2"},
     traffic={"timeout": 10},
 )
 class Kotest:
-    # Declare the model as a class attribute
+    # Deklaracja modelu jako atrybut klasy
     bento_model = BentoModel("kotest:latest")
 
     def __init__(self):
         self.model = bentoml.mlflow.load_model(self.bento_model)
 
-    @bentoml.api
-    def predict(self, input_data: np.ndarray) -> list[str]:
-        preds = self.model.predict(input_data)
-        return [target_names[i] for i in preds]
+    def preprocess_image(self, image_data: bytes) -> np.ndarray:
+        image = Image.open(io.BytesIO(image_data)).convert("L")
+        image_tensor = transform(image).unsqueeze(0)
+        return image_tensor.numpy()
 
-#todo sprawdzić czy to będzie dobry input, czy nie trzeba jakiejś dziwnej struktury danych na ten obrazek mieć
+    @bentoml.api
+    def predict(self, input_data: str) -> list:
+        """
+        Oczekuje wejścia jako base64-encoded string.
+        """
+        try:
+            image_bytes = base64.b64decode(input_data)
+            processed_image = self.preprocess_image(image_bytes)
+            preds = self.model.predict(processed_image)
+            return preds.tolist()
+        except Exception as e:
+            return {"error": str(e)}
