6cb3ba5787d241e087ab457c9fc95f68c9a0152b
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-03-31 13:30:53 +0200

    chat saves

--- Deploy/src/service.py
+++ Deploy/src/service.py
@@ -1,11 +1,9 @@
 import bentoml
 import io
 from PIL import Image
-import numpy as np
 import torch
 import torchvision.transforms as transforms
-from bentoml.models import BentoModel
-from bentoml.io import Image as BentoImage
+from bentoml.io import Image as BentoImage, JSON
 
 # Transformacja obrazu
 transform = transforms.Compose([
@@ -15,25 +13,24 @@ transform = transforms.Compose([
     transforms.Normalize((0.5,), (0.5,))
 ])
 
+# Definicja serwisu w BentoML
 @bentoml.service()
 class Kotest:
-    bento_model = BentoModel("kotest:latest")
-
     def __init__(self):
-        self.model = bentoml.mlflow.load_model(self.bento_model)
+        self.model = bentoml.mlflow.load_model("kotest:latest")
 
     def preprocess_image(self, image: Image.Image) -> torch.Tensor:
         image = transform(image).unsqueeze(0)
         return image
 
-    @bentoml.api(input=BentoImage(), output=bentoml.io.JSON())
-    def predict(self, image: Image.Image):
+    @bentoml.api(input_spec=BentoImage, output_spec=JSON)
+    async def predict(self, image: Image.Image):
         """
         Oczekuje obrazu jako pliku przesłanego w multipart/form-data.
         """
         try:
             processed_image = self.preprocess_image(image)
-            pred = self.model.predict(processed_image)
+            pred = self.model(processed_image)
             return {"prediction": pred.tolist()}
         except Exception as e:
             return {"error": str(e)}
